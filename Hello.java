c5f70b8bf62a5c9e83fcacdc170cf25ccfaf5396
Raghu Kotamraju <nani.nandan05@gmail.com>
2023-01-09 23:57:01 -0600

    HELLO PROJECT

---
+++ Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HOW ARE YOU?");
+		System.out.println("Hello World");
+		System.out.println("I KNOW WHAT ARE YOU DOING?");
+
+	}
+
+}
